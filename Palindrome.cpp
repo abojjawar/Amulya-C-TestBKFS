6de8fceb0e1010caa99a6cb83ab55f92a4f12c6a
abojjawar <70165172+abojjawar@users.noreply.github.com>
2020-08-25 22:14:06 +0530

    Create Palindrome.cpp

---
+++ Palindrome.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+bool isPalindrome(const string &str)
+{
+   if (str.empty())
+      return false;
+   int i =0;
+   int j = str.length() -1;
+   
+   while( i< j)
+   {
+      if(str[i++] != str [j--])
+        return false;
+   }
+   return true;
+ }
+ 
+ int main()
+ {
+    //  check for palindrome on sample string
+    string s = "DAD"
+    if(isPalindrome(s)
+       std::cout << given string is palindrome" <<std::endl;
+    else
+       std::cout << given string is not palindrome" <<std::endl;
+       
+    return 0 ;
+ }
